26d934028f9ff0a5128bbb5657abd48cc9ed1f16
OCorvo <104469048+IsCorvo@users.noreply.github.com>
2023-01-23 17:14:30 +0000

    Commit EstDados

--- EstruturaDeDados/EstDados.js
+++ EstruturaDeDados/EstDados.js
@@ -1,3 +1,27 @@
 const pilotos = [
-    'Senna', 'Prost', 'S'
+    'Senna', 'Prost', 'Schumacher', 'Hamilton'
 ]
+
+// A indexação (index) começa pelo número 0
+console.log(pilotos[0]) // Senna
+console.log(pilotos[3]) // Hamilton
+
+// Acessar o tamanho do array
+console.log(pilotos.length)
+
+// Iterável
+for (let piloto of pilotos) {
+    console.log(piloto)
+}
+
+// Adicionar elementos
+pilotos.push('Alonso')
+console.log(pilotos)
+
+// Encontrar um elemento
+const senna = pilotos.find(piloto => piloto === 'Senna')
+console.log(senna)
+
+// Deletar um elemento
+pilotos.splice(1, 1)
+console.log(pilotos)
